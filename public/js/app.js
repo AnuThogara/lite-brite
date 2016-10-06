b2e218b0e948ed6853691507e90bb7ea6a08d3f8
AnuThogara <anuradha.thogara@gmail.com>
2016-10-05 20:27:55 -0400

    changes made to grid

--- public/js/app.js
+++ public/js/app.js
@@ -1,17 +1,26 @@
-(function() {//iife
+(function() { //iife
 
-  var numberOfRows = 40;
-  var numberOfCols = 40;//number of columns in a grid
-  var canvas= $('#canvas');//my placement area thiunk of paper drawing
-  // let's make some rows and put them in the body
-  for(var rowCount = 0; rowCount < numberOfRows; rowCount +=1 ){
-    var row = $('<tr> </tr>');
-    for(var colCount=0; colCount< numberOfCols; colCount+=1){
-      var column = $('<td> </td>');
-    //column inside a row
-    column.addClass('cell');
-    row.append(column);
-  }
-    canvas.append(row);
-}
+    var numberOfRows = 15;
+    var numberOfCols = 15; //number of columns in a grid
+    var canvas = $('#canvas'); //my placement area thiunk of paper drawing
+    // let's make some rows and put them in the body
+    makeGrid();
+
+    function changeColor(event){
+
+      $(this).css('background-color','red');
+    }
+    function makeGrid() {
+        for (var rowCount = 0; rowCount < numberOfRows; rowCount += 1) {
+            var row = $('<tr> </tr>');
+            for (var colCount = 0; colCount < numberOfCols; colCount += 1) {
+                var column = $('<td> </td>');
+                //column inside a row
+                column.addClass('cell');
+                row.append(column);
+            }
+            canvas.append(row);
+        }
+    }
+
 }());
