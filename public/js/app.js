5afbb8d05f03dcdd15ee442d45bd47d2eb335b44
AnuThogara <anuradha.thogara@gmail.com>
2016-10-10 19:00:33 -0400

    refactored makeGrid function to empty old grid

--- public/js/app.js
+++ public/js/app.js
@@ -1,16 +1,23 @@
 (function() { //iife
 
-    var numberOfRows = 15;
-    var numberOfCols = 15; //number of columns in a grid
+    // var numberOfRows = 15;
+    // var numberOfCols = 15; //number of columns in a grid
     var canvas = $('#canvas'); //my placement area thiunk of paper drawing
     // let's make some rows and put them in the body
-    makeGrid();
+    makeGrid(15,15);
+    clearGrid();
+    makeGrid(30,30);
+    $('.cell').on('click', changeColor);
+
+    function clearGrid(){
+      canvas.empty();
+    }
 
     function changeColor(event){
-
-      $(this).css('background-color','red');
+      $(this).toggleClass('red');
     }
-    function makeGrid() {
+
+    function makeGrid(numberOfRows, numberOfCols) {
         for (var rowCount = 0; rowCount < numberOfRows; rowCount += 1) {
             var row = $('<tr> </tr>');
             for (var colCount = 0; colCount < numberOfCols; colCount += 1) {
